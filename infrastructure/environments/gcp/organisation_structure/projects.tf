7573da11674e545c561f75a604065a9841bc894a
Jonathan Rainer <Jonathan@jonathan-rainer.com>
2025-10-16 09:31:12 +0100

    Add frontdoor Terraform resources (#5)
    * Add frontdoor Terraform resources
    
    * Move to declaring projects centrally
    
    * Remove unnecessary provider block
    
    * Use a proper reference
    
    * Add frontdoor resources that do nothing

---
+++ infrastructure/environments/gcp/organisation_structure/projects.tf
@@ -0,0 +1,19 @@
+locals {
+  projects = [
+    {
+      name : "frontdoor"
+      display_name : "Frontdoor Service"
+      parent_folder : google_folder.services.id
+    }
+  ]
+}
+
+resource "google_project" "airdate_project" {
+  for_each = {
+    for index, project in local.projects :
+    project.name => project
+  }
+  name       = each.value.display_name
+  project_id = each.value.name
+  folder_id  = each.value.parent_folder
+}
