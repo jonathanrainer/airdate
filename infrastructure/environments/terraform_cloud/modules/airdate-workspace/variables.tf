e36423ed8215435aefc530b4402556b14d19d537
jonathanrainer <jonathan.rainer@apollographql.com>
2025-10-16 09:14:10 +0100

    Make module level changes so changes applied manually are persisted

--- infrastructure/environments/terraform_cloud/modules/airdate-workspace/variables.tf
+++ infrastructure/environments/terraform_cloud/modules/airdate-workspace/variables.tf
@@ -9,3 +9,11 @@ variable "organization_name" {
 variable "workload_identity_variable_set_id" {
   type = string
 }
+
+variable "vcs_integration" {
+  type = bool
+}
+
+variable "working_directory" {
+  type = string
+}
--- infrastructure/environments/terraform_cloud/workspaces.tf
+++ infrastructure/environments/terraform_cloud/workspaces.tf
@@ -1,15 +1,28 @@
 locals {
   workspaces = [
-    "gcp-organisation-structure",
-    "frontdoor"
+    {
+      name: "gcp-organisation-structure",
+      vcs_integration: true
+      working_directory: "infrastructure/environments/gcp/organisation_structure"
+    },
+    {
+      name: "frontdoor",
+      vcs_integration: true
+      working_directory: "infrastructure/environments/gcp/frontdoor"
+    }
   ]
 }
 
 module "terraform_cloud_workspaces" {
   source   = "./modules/airdate-workspace"
-  for_each = toset(local.workspaces)
+  for_each = {
+    for index, workspace in local.workspaces:
+    workspace.name => workspace
+  }
 
   organization_name                 = tfe_organization.pmqs_cloud.name
-  workspace_name                    = each.value
+  workspace_name                    = each.value.name
   workload_identity_variable_set_id = tfe_variable_set.gcp_workload_identity_federation.id
+  vcs_integration = each.value.vcs_integration
+  working_directory = each.value.working_directory
 }
