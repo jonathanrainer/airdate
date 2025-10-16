6ff05537f01046ead67e5aba29ef62b4806d892d
Jonathan Rainer <Jonathan@jonathan-rainer.com>
2025-10-16 09:17:50 +0100

    Make module level changes so changes applied manually are persisted (#4)

--- infrastructure/environments/terraform_cloud/modules/airdate-workspace/workspace.tf
+++ infrastructure/environments/terraform_cloud/modules/airdate-workspace/workspace.tf
@@ -1,4 +1,15 @@
 resource "tfe_workspace" "workspace" {
   name         = var.workspace_name
   organization = var.organization_name
+  working_directory = var.working_directory
+  file_triggers_enabled = false
+  dynamic vcs_repo {
+    for_each = var.vcs_integration ? [{}] : []
+    content {
+      identifier = "jonathanrainer/airdate"
+      branch = "main"
+      github_app_installation_id = "ghain-LBfvqR9WHhE8quWk"
+      ingress_submodules = false
+    }
+  }
 }
