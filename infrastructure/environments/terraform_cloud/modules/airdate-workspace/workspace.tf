788bd42d36858327db2b0e84e998e9b3f8a7c03b
Jonathan Rainer <Jonathan@jonathan-rainer.com>
2025-10-18 22:00:20 +0100

    Add auto-apply (#8)

--- infrastructure/environments/terraform_cloud/modules/airdate-workspace/workspace.tf
+++ infrastructure/environments/terraform_cloud/modules/airdate-workspace/workspace.tf
@@ -1,15 +1,16 @@
 resource "tfe_workspace" "workspace" {
-  name         = var.workspace_name
-  organization = var.organization_name
-  working_directory = var.working_directory
+  name                  = var.workspace_name
+  organization          = var.organization_name
+  working_directory     = var.working_directory
   file_triggers_enabled = false
-  dynamic vcs_repo {
+  dynamic "vcs_repo" {
     for_each = var.vcs_integration ? [{}] : []
     content {
-      identifier = "jonathanrainer/airdate"
-      branch = "main"
+      identifier                 = "jonathanrainer/airdate"
+      branch                     = "main"
       github_app_installation_id = "ghain-LBfvqR9WHhE8quWk"
-      ingress_submodules = false
+      ingress_submodules         = false
+      auto_apply                 = true
     }
   }
 }
