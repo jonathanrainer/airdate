b2a38e97b06184331acc47f020175d4933c1f15f
jonathanrainer <jonathan.rainer@apollographql.com>
2025-10-18 22:34:22 +0100

    Add organisation level permissions for new account

---
+++ infrastructure/environments/gcp/organisation_structure/organisation_iam.tf
@@ -0,0 +1,22 @@
+data "google_iam_role" "folder_viewer" {
+  name = "roles/resourcemanager.folderViewer"
+}
+resource "google_organization_iam_binding" "folder_viewer" {
+  members = [
+    "user:jonathan@airdate.cloud"
+  ]
+  org_id = data.google_organization.airdate.id
+  role   = data.google_iam_role.folder_viewer
+}
+
+data "google_iam_role" "browser" {
+  name = "roles/browser"
+}
+
+resource "google_organization_iam_binding" "browser" {
+  members = [
+    "user:jonathan@airdate.cloud"
+  ]
+  org_id = data.google_organization.airdate.id
+  role   = data.google_iam_role.browser
+}
