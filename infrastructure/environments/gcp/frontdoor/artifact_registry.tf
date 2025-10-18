8ba6b013c87170563d4dee71e3965045712d15a1
jonathanrainer <jonathan.rainer@apollographql.com>
2025-10-18 21:47:02 +0100

    Create Artifact Repository for Frontdoor images

---
+++ infrastructure/environments/gcp/frontdoor/artifact_registry.tf
@@ -0,0 +1,34 @@
+terraform {
+  required_providers {
+    google = {
+      source  = "hashicorp/google"
+      version = "6.38.0"
+    }
+  }
+}
+data "google_iam_role" "artifact_registry_reader" {
+  name = "roles/artifactregistry.createOnPushWriter)"
+}
+
+resource "google_artifact_registry_repository" "frontdoor_repo" {
+  location      = "us-central1"
+  repository_id = "frontdoor"
+  description   = "Image repository for the Frontdoor Service Docker Images"
+  format        = "DOCKER"
+
+  docker_config {
+    immutable_tags = true
+  }
+}
+
+resource "google_service_account" "frontdoor_artifact_repository_writer" {
+  account_id = "frontdoor-artifact-repo-writer"
+}
+
+resource "google_artifact_registry_repository_iam_binding" "writer" {
+  members = [
+    google_service_account.frontdoor_artifact_repository_writer.member
+  ]
+  repository = google_artifact_registry_repository.frontdoor_repo.name
+  role       = data.google_iam_role.artifact_registry_reader.name
+}
---
+++ infrastructure/environments/gcp/frontdoor/workload_identity.tf
@@ -0,0 +1,36 @@
+data "google_iam_role" "workload_identity_user" {
+  name = "roles/iam.workloadIdentityUser"
+}
+
+data "google_project" "project" {}
+
+resource "google_iam_workload_identity_pool" "github_actions" {
+  workload_identity_pool_id = "github-actions"
+  display_name              = "GitHub Actons"
+  description               = "Identity pool to authenticate GitHub Actions from the `airdate` repo"
+}
+
+resource "google_iam_workload_identity_pool_provider" "github_actions" {
+  workload_identity_pool_id          = google_iam_workload_identity_pool.github_actions.workload_identity_pool_id
+  workload_identity_pool_provider_id = "github-actions"
+  display_name                       = "GitHub Actions"
+  description                        = "Identity pool provider for GitHub Actions to allow pushing to GAR"
+  oidc {
+    issuer_uri = "https://token.actions.githubusercontent.com"
+  }
+  attribute_mapping = {
+    "google.subject" : "assertion.sub"
+    "attribute.actor" : "assertion.actor"
+    "attribute.repository" : "assertion.repository"
+    "attribute.repository_owner" : "assertion.repository_owner"
+  }
+  attribute_condition = "assertion.repository_owner == 'jonathanrainer' && assertion.repository == 'airdate'"
+}
+
+resource "google_service_account_iam_binding" "terraform_workload_identity_user" {
+  members = [
+    "principalSet://iam.googleapis.com/projects/${data.google_project.project.number}/locations/global/workloadIdentityPools/${google_iam_workload_identity_pool.github_actions.workload_identity_pool_id}/attribute.repository/jonathanrainer/airdate"
+  ]
+  role               = data.google_iam_role.workload_identity_user.name
+  service_account_id = google_service_account.frontdoor_artifact_repository_writer.id
+}
